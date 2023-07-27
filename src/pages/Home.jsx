2812c5acafe9bbd8234244b468ee1c5c4dcac29c
orahbaloyi1 <orahbaloyi1@gmail.com>
2023-07-27 10:36:54 +0200

    Search functionality added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import HeroSec from "../components/HeroSec";
-import FeaturedJobs from "../components/FeaturedJobs";
 import MainLayout from "../layouts/MainLayouts";
 
 const Home = () => {
   return (
     <MainLayout>
       <HeroSec />
-      <FeaturedJobs />
     </MainLayout>
   );
 };
